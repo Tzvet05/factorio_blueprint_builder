4a92e92b345ce756860cafd8db0b3e339dfe8674
Tzvet05 <tzvetan.trave@abadcafe.org>
2024-04-30 22:33:23 +0200

    tweaked main.c

--- src/main.c
+++ src/main.c
@@ -4,15 +4,10 @@
 
 int	main(int argc, char **argv)
 {
-	char	error;
-
 	if (argc != 3)
 	{
 		puterr(ERRORSTRING_AMOUNT_ARGS, NULL);
 		exit(ERRORCODE_AMOUNT_ARGS);
 	}
-	error = file_to_blueprint(argv[1], argv[2]);
-	if (error != 0)
-		exit(error);
-	exit(0);
+	exit(file_to_blueprint(argv[1], argv[2]));
 }
